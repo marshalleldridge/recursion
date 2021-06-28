55d49f8a2bc486e5f20110924387f998aa330b6f
Marshall Eldridge <l2marshall@gmail.com>
2021-06-28 16:09:05 -0600

    Implemented factorial unit tests.

--- src/main/java/edu/cnm/deepdive/Factorials.java
+++ src/main/java/edu/cnm/deepdive/Factorials.java
@@ -2,12 +2,12 @@ package edu.cnm.deepdive;
 
 public class Factorials {
 
-  public static long computeRecursive(int num) {
-    if (num<0) {
+  public static long computeRecursive(int n) {
+    if (n < 0) {
       throw new IllegalArgumentException();
     }
 
-    return (num == 0) ? 1 : (num * computeRecursive(num-1));
+    return (n == 0) ? 1 : (n * computeRecursive(n - 1));
   }
-  
+
 }
